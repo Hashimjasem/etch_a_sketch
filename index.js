1a4aa8031b9e75ecaa8e01afe9736f4278c35112
Hashimjasem <j_hashim1305@outlook.com>
2023-10-15 20:36:14 +0800

    20mins

---
+++ index.js
@@ -0,0 +1,13 @@
+// generate a grid
+const gridContainer = document.getElementById("container")
+
+let gridsquare = "<div class='box'>.</div>"
+
+let grid = function(gridContainer){
+    // how do i create multiple divs
+    function creategridsquare(){
+    for (let i=0; 1 < 80; i++) {
+        gridContainer.insertAdjacentHTML("afterend, html")
+    }}
+
+}
