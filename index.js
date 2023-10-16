4e6d8e905bb9fc7c5954aefbb958446b87d956b5
Hashimjasem <j_hashim1305@outlook.com>
2023-10-16 22:36:41 +0800

    11515

--- index.js
+++ index.js
@@ -1,13 +1,61 @@
-// generate a grid
-const gridContainer = document.getElementById("container")
+document.addEventListener("DOMContentLoaded", function () {
+getSize()
+// createGrid(32)
+console.log('loaded')
 
-let gridsquare = "<div class='box'>.</div>"
+})
 
-let grid = function(gridContainer){
-    // how do i create multiple divs
-    function creategridsquare(){
-    for (let i=0; 1 < 80; i++) {
-        gridContainer.insertAdjacentHTML("afterend, html")
-    }}
 
+
+
+
+
+function createGrid(size) {
+    let grid = document.querySelector('.grid');
+
+    grid.getElementsByClassName.gridTemplateColumns = `repeat(${size}, 1fr)`
+    grid.getElementsByClassName.gridTemplateRows = `repeat(${size}, 1fr)`
+
+    for (var rows = 0; rows < size; rows++) {
+        for (var columns = 0; columns < size; columns++) {
+            let div = document.createElement("div");
+            div.style.backgroundColor = "yellow"
+            grid.insertAdjacentElement("beforeend", div)
+
+        };
+    };
+};
+
+function getSize(){
+    let input = prompt("how many squares per side")
+    let message = document.querySelector("message")
+    if (input == ""){
+        message.innerHTML = "Please provide a number"    }
+    else if(input < 0 || input < 100) {
+        createGrid(size)
+    }
 }
+
+// function clearGrid() {
+//     box.remove();
+// };
+
+// function refreshGrid() {
+//     var z = prompt("How many boxes per side?");
+//     clearGrid();
+//     createGrid(z);
+// };
+
+// createGrid(20);
+
+// box.mouseover(function () {
+//     this.css("background-color", "black");
+// });
+
+// $(".newGrid").click(function () {
+//     refreshGrid();
+
+//     box.mouseover(function () {
+//         this.css("background-color", "black");
+//     });
+// });
